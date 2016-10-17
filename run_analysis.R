861bfc614c9069e2c711a3ce4d3b96528c9a1e2c
gRittyKitty <ari.pine@tradingtechnologies.com>
2016-10-17 00:37:58 -0400

    completed 1-4

--- run_analysis.R
+++ run_analysis.R
@@ -2,19 +2,42 @@
 # Getting and Cleaning Data
 # Final Project
 
+library(readr)
+library(dplyr)
+
 #   *****
 #   *****   1. Merge training and test data sets to create one data set
 #   *****
-# first set the path for the training and data files
+# first set the path 
 # then load in the data sets
+
+# First load in all data associated with training
+# I use fname as a temporary variable for the file name of what is read in
 core_path = "/users/apine/documents/datacourse/cleaningdata/uci_dataset/"
 fname <- paste(core_path,"train/X_train.txt",sep="")
-wearable_set <- read.table(fname, colClasses = "character") # this is just the training set for now but it will hold the entire set
+train_set <- read.table(fname, colClasses = "character") # this is just the training set for now but it will hold the entire set
 fname <- paste(core_path,"test/X_test.txt",sep="")
-test_set <- read.table(fname,colClasses = "character")
+test_set <- read.table(fname)
 # now combine the data sets into one dataframe
-wearable_set <- rbind(wearable_set,test_set)
-# 1 is complete. both data sets are combined into one large data set of 561 columns
+wearable_set <- rbind(train_set,test_set)
+# lets now add in the activity data
+fname <- paste(core_path,"train/y_train.txt",sep="")
+train_activity_set <- read.table(fname)
+fname <- paste(core_path,"test/y_test.txt",sep="")
+test_activity_set <- read.table(fname)
+activity_set <- rbind(train_activity_set,test_activity_set)
+colnames(activity_set)="activity"
+wearable_set = cbind(wearable_set,activity_set)
+# lets now add in the subject data
+fname <- paste(core_path,"train/subject_train.txt",sep="")
+train_subject_set <- read.table(fname)
+fname <- paste(core_path,"test/subject_test.txt",sep="")
+test_subject_set <- read.table(fname)
+subject_set = rbind(train_subject_set,test_subject_set)
+colnames(subject_set)="subject"
+wearable_set = cbind(wearable_set,subject_set)
+# 1 is complete. all data sets are combined into one large data set 
+# named wearable_set
 
 #   *****
 #   *****   2. extracts only the measurments on the mean and std dev
@@ -25,11 +48,36 @@ wearable_set <- rbind(wearable_set,test_set)
 fname <- paste(core_path,"features.txt",sep="")
 features = read.table(fname)
 # may as well add actual column names to the data set
+# NOTE that this is actually most of #4
 colnames(wearable_set)=features$V2
 # now a list of columns that contains "mean" or "std"
 col_list <- grep("mean|std",features$V2)
 pruned_data <- wearable_set[,col_list]
 #   2: pruned_data has only the measurments on the mean and std dev for each measurement
+# but we eliminated the columns for activity and subject
+pruned_data <- cbind(pruned_data, activity_set, subject_set)
+
+
+#   *****
+#   *****   3. Use descriptive activity names to name activities in data set
+#   *****
+#   first get labels
+fname <- paste(core_path,"activity_labels.txt",sep="")
+labels <- read.table(fname)
+colnames(labels)[1]="activity"  # this is done to have an appropriate merge
+labeled_set <- merge(pruned_data,labels)
+# labeled_set is the data set and now contains the activities as descriptive names
+
+#   *****
+#   *****   4. Appropriately labels the data set with descriptive variable names
+#   *****
+#  Nearly all of this has already been done in the preceding steps
+# The one 'v' column has the descriptive labels that we just added
+colnames(labeled_set)[82] = "activity"
+
+# ****
+# labeled_set is the data set that has the data for #4
+# ****
 
 
 
